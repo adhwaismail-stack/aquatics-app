3e9e9ea769cb98e42fa93ad33118714de017e1e0
Adhwa Ismail <muhammadadhwa@gmail.com>
2026-04-22 21:36:53 +0800

    Fix Stripe API version

--- app/api/portal/route.ts
+++ app/api/portal/route.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-01-27.acacia'
+apiVersion: '2026-03-25.basil'
 })
 
 const supabase = createClient(
